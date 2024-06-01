ee4df77906e8d5b600185716f9b3a374dc7ae642
Thales Mattos <103903195+ThalesMattos@users.noreply.github.com>
2024-06-01 13:50:54 -0300

    Delete Servico-de-email directory

--- Servico-de-email/src/main/java/com/thalesmattos/servicodeemail/adapters/IEmailSenderGateway.java
+++
@@ -1,5 +0,0 @@
-package com.thalesmattos.servicodeemail.adapters;
-
-public interface IEmailSenderGateway {
-    void sendEmail (String to, String subject, String body);
-}
--- Servico-de-email/src/main/java/com/thalesmattos/servicodeemail/core/IEmailSenderUseCase.java
+++
@@ -1,5 +0,0 @@
-package com.thalesmattos.servicodeemail.core;
-
-public interface IEmailSenderUseCase {
-    void sendEmail(String to, String subject, String body);
-}
